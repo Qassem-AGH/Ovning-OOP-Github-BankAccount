32a0ddf17acb1d50b14bfad30aaf673c0f335701
qassem <qassem.abdulghani@student.nbi-handelsakademi>
2025-09-22 23:42:58 +0200

    Arv: Underklass för sparkonto tillagd

---
+++ SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_OOP_Github_BankAccount
+{
+    public class SavingsAccount : BankAccount
+    {
+        public double InterestRate { get; set; }
+
+        public void ApplyInterest()
+        {
+            Deposit(GetBalance() * InterestRate);
+        }
+    }
+
+}
