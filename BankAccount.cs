5c242c17bce64c39c868ba29f9c79e88ca3bd1d1
qassem <qassem.abdulghani@student.nbi-handelsakademi>
2025-09-22 23:35:49 +0200

    Inledande grundläggande BankAccount-klass utan OOP-principer

---
+++ BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_OOP_Github_BankAccount
+{
+    // BankAccount.cs
+    public class BankAccount
+    {
+        public string AccountHolder;
+        public double Balance;
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Balance: {Balance} Kr");
+        }
+    }
+
+}
